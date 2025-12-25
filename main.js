1f14dbeeb7cbeff6647f2fd27ef540e2b2cbd8c2
MaiNT <mai28022000@gmail.com>
2025-12-25 10:07:48 +0700

    add log function

--- main.js
+++ main.js
@@ -3,11 +3,9 @@ console.log("hi");
 var a = 10;
 console.log("conflict test hien branch");
 
-<<<<<<< HEAD
 function sum(a, b) {
   return a + b;
-=======
+}
 function log() {
   console.log("hi");
->>>>>>> hien
 }
